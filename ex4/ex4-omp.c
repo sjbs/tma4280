a527e3d3cad8f404f97cea9721f3e4647f0dd59b
silas <silas.spence@gmail.com>
2013-02-15 23:10:48 +0100

    Unified serial/omp idea dropped. Wall time printout added.

--- ex4/ex4-omp.c
+++ ex4/ex4-omp.c
@@ -21,20 +21,30 @@ int main(int argc, char** argv)
 	double sum=0;
 	
 	Vector v = createVector(N);
+
+	start=WallTime();
 	
-#pragma omp parallel for schedule(static) reduction(+:v,sum)
+	#pragma omp parallel for schedule(static) reduction(+:sum)
 	for (long int i=0;i<N;++i) {
-		double temp=1.0/((i+1)*(i+1));
+		double temp=1.0/((double)(i+1)*(double)(i+1));
 		v->data[i] = temp;	
 		sum += temp;
 	}
-	
+
+// 	double sumV = 0;	
+// 	for (long int i=0;i<N;++i) {
+// 		sumV += (*v).data[i];
+// 	}
+
+	end=WallTime();
+
 	//exact value of pi
 	double pi=(4.0*atan(1.0));
 	
+//	printf("sum: %f\n", sumV);
 	printf("sum: %f\n", sum);
 	printf("difference: %1.16f\n", pi*pi/6.0-sum);
-	
+	printf("Wall Time is %1.4f\n",)
 }
 	
 	
