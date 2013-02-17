5191feb724b7db615fafa2daec8b33f904db62b7
silas <silas@silas-VirtualBox.(none)>
2013-02-17 16:57:33 +0100

    Rolled back ex4-serial.c to remove mp stuff that had been overwritten into file. First commit from windows vm. whee

--- ex4/ex4-serial.c
+++ ex4/ex4-serial.c
@@ -1,55 +1,41 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
-#include "mpi.h"
 #include "common.h"
 
-int main(int argc, char **argv)
+
+int main(int argc, char** argv)
 {
-	double mysum, sum, x, pi, t1, t2;
-	int n, myid, nproc, i;
-	MPI_Init (&argc, &argv);
-	MPI_Comm_size (MPI_COMM_WORLD, &nproc);
-	MPI_Comm_rank (MPI_COMM_WORLD, &myid);
-    
-	//printf("comm_size is %i \n", nproc);
-	//printf("comm_rank is %i \n", myid);
-    
-	//ask about input - arg vs scanf
-	if (myid == 0) {
-		printf (" Enter vector length:\n");
-		scanf ("%i",&n);
-		if (n<=){
-			printf("need a positive vector length, idiot.");
-			MPI_Finalize();
-			return 1;
-		}
-		t1 = MPI_Wtime();
-	}
-	MPI_Bcast (&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
-	
-	sum = 0.0;
-	for (i = myid*n/nproc; i <= (myid+1)*n/nproc; i ++) {
-		double temp=1.0/((double)(i+1)*(double)(i+1));
-		//v->data[i] = temp;	
-		mysum += temp;
-	}
-    
-	MPI_Reduce (&mysum, &sum, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
-    
-	if (myid == 0) {
-		t2 = MPI_Wtime();
-		pi = 4.0 * atan(1.0);
-		printf("sum: %f\n", sum);
-		printf("difference: %1.16f\n", pi*pi/6.0-sum);
-		printf("Wall Time is %1.4f\n",t2-t1);
+	if (argc < 2) {
+		printf("Need n, number of vector elements. Tard.\n");
+		return 1;
 	}
 	
-	MPI_Finalize();
-	return(0);
+	int N=atoi(argv[1]);
+	
+	if (N<=0) {
+		printf("Need a positive vector length, mormon.\n");
+		return 1;
+	}
+	
+	double sum=0;
+	
+	Vector v = createVector(N);
+	
+	for (long int i=0;i<N;++i) {
+		double temp=1.0/((i+1)*(i+1));
+		v->data[i] = temp;	
+		sum += temp;
+	}
+	
+	//exact value of pi
+	double pi=(4.0*atan(1.0));
+	
+	printf("sum: %f\n", sum);
+	printf("difference: %1.16f\n", pi*pi/6.0-sum);
+	
 }
-
-
+	
 	
 
   
