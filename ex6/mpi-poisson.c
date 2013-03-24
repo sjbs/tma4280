66cc05930927bd63de6f1434f3c2f57e91279bf8
silas vm <silas.spence@gmail.com>
2013-03-24 12:37:21 +0100

    tranpose now working. fst not, suspect feeding non-contiguous blocks, somehow.

--- ex6/mpi-poisson.c
+++ ex6/mpi-poisson.c
@@ -26,7 +26,7 @@ Real *createRealArray (int n);
 int *createIntArray (int n);
 Real **createReal2DArray (int m, int n);
 void transpose (Real **bt, Real **b, int m, int mglob, Real *sendbuf, 
-    Real *recbuf, int *sendcnt, int *sdispl,int rank);
+    Real *recbuf, int *sendcnt, int *sdispl,int rank, int *cols, int size);
 void fst_(Real *v, int *n, Real *w, int *nn);
 void fstinv_(Real *v, int *n, Real *w, int *nn);
 void splitVector(int globLen, int size, int** len, int** displ);
@@ -65,10 +65,13 @@ int main(int argc, char **argv )
 
   m=cols[rank];
   int localdof = m*mglob;
-  printf("localdof[%i]=%i\n",rank,localdof);
+  //printf("localdof[%i]=%i\n",rank,localdof);
   //nn=4*nlocal;
 
   //printf("%i\n",ofs[rank]);
+  //printf("%i\n",size);
+  //printf("%i,%i\n",rank,m);
+
 
   diag = createRealArray (mglob);
   b    = createReal2DArray (mglob,m);
@@ -81,16 +84,16 @@ int main(int argc, char **argv )
 
   for (i=0; i<size; i++){
     sendcnt[i]=cols[rank]*cols[i];
-    if (rank==1) printf("sendcnt=%i  ",sendcnt[i]);
+    //if (rank==1) printf("sendcnt=%i  ",sendcnt[i]);
   }
-  if (rank==1) printf("\n");
+  //if (rank==1) printf("\n");
 
   sdispl[0]=0;
   for (i=1; i<size; i++){
     sdispl[i]=sdispl[i-1]+sendcnt[i-1];
-    if (rank==1) printf("sdispl=%i  ",sdispl[i]);
+    //if (rank==1) printf("sdispl=%i  ",sdispl[i]);
   }
-  if (rank==1) printf("\n");
+  //if (rank==1) printf("\n");
   
   h    = 1./(Real)n;
   pi   = 4.*atan(1.);
@@ -100,77 +103,85 @@ int main(int argc, char **argv )
   }
   for (j=0; j < mglob; j++) {
     for (i=0; i < m; i++) {
-      b[j][i] = (i+1)*(j+2)-rank;
-      if (rank==1) printf("b[%i][%i]=%f\t",j,i,b[j][i]);
+      b[j][i] = (i+1+ofs[rank])*(j+2);
+      if (rank==0) printf("b[%i][%i]=%2.1f\t",j,i,b[j][i]);
     }
-    if (rank==1) printf("\n");
+    if (rank==0) printf("\n");
   }
   
   for (j=0; j < m; j++) {
-    //fst_(b[j], &n, z, &nn);
+    fst_(b[j], &n, z, &nn);
   }
 
-  transpose (bt,b,m,mglob,sendbuf,recbuf,sendcnt,sdispl,rank);
+  transpose (bt,b,m,mglob,sendbuf,recbuf,sendcnt,sdispl,rank, cols, size);
 
-  // for (i=0; i < m; i++) {
-  //  fstinv_(bt[i], &n, z, &nn);
-  // }
+  for (i=0; i < m; i++) {
+   fstinv_(bt[i], &n, z, &nn);
+  }
   
-  // for (j=0; j < mglob; j++) {
-  //  for (i=0; i < m; i++) {
-  //    bt[j][i] = bt[j][i]/(diag[i]+diag[j]);
-  //   }
-  // }
+  for (j=0; j < mglob; j++) {
+   for (i=0; i < m; i++) {
+     bt[j][i] = bt[j][i]/(diag[i]+diag[j]);
+    }
+  }
   
-  // for (i=0; i < m; i++) {
-  //   fst_(bt[i], &n, z, &nn);
-  // }
+  for (i=0; i < m; i++) {
+    fst_(bt[i], &n, z, &nn);
+  }
 
-  // transpose (bt,b,m,mglob,sendbuf,recbuf,sendcnt,sdispl);
+  transpose (bt,b,m,mglob,sendbuf,recbuf,sendcnt,sdispl,rank, cols, size);
 
-  // for (j=0; j < m; j++) {
-  //   fstinv_(b[j], &n, z, &nn);
-  // }
+  for (j=0; j < m; j++) {
+    fstinv_(b[j], &n, z, &nn);
+  }
 
-  // umax = 0.0;
-  // for (j=0; j < mglob; j++) {
-  //   for (i=0; i < m; i++) {
-  //     if (b[j][i] > umax) umax = b[j][i];
-  //   }
-  // }
-  // printf (" umax = %e \n",umax);
+  umax = 0.0;
+  for (j=0; j < mglob; j++) {
+    for (i=0; i < m; i++) {
+      if (b[j][i] > umax) umax = b[j][i];
+    }
+  }
+  printf (" umax = %e \n",umax);
 
   MPI_Finalize();
   return 0;
 }
 
 void transpose (Real **bt, Real **b, int m, int mglob, Real *sendbuf,
-  Real *recbuf,int *sendcnt, int *sdispl,int rank)
+  Real *recbuf,int *sendcnt, int *sdispl,int rank, int *cols, int size)
 {
-  for (int j=0; j < mglob; j++) {
+  int ind=0;
+  int joff=0;
+  for (int p=0; p<size; p++){
     for (int i=0; i < m; i++) {
-      int ind=j*m+i;
-      sendbuf[ind]=b[j][i];
-      if (rank==1) printf("%2.1f  ",sendbuf[ind]);
+      for (int j=0; j < cols[p]; j++) {
+        //if (rank==1) printf("%i, %i, %2.1f\n",j+joff,i,b[j+joff][i]);
+        sendbuf[ind]=b[j+joff][i];
+        //if (rank==1) printf("%i\n",ind);
+        //if (rank==1) printf("%2.1f  ",sendbuf[ind]);
+        ind++;
+      }
     }
+    joff=joff+cols[p];
+    //if (rank==1) printf("%i\n",joff);
   }
-  if (rank==1) printf("\n\n");
+  //if (rank==1) printf("\n\n");
 
   MPI_Alltoallv(sendbuf, sendcnt, sdispl,MPI_DOUBLE,
      recbuf, sendcnt, sdispl,MPI_DOUBLE,MPI_COMM_WORLD);
   
-  for (int i=0;i<m*mglob;i++) {
-    if (rank==1) printf("%2.1f  ",recbuf[i]);
-  }
-  if (rank==1) printf("\n\n");
+  // for (int i=0;i<m*mglob;i++) {
+  //   if (rank==1) printf("%2.1f  ",recbuf[i]);
+  // }
+  // if (rank==1) printf("\n\n");
 
   for (int j=0; j < mglob; j++) {
     for (int i=0; i < m; i++) {
       int ind=j*m+i;
       bt[j][i]=recbuf[ind];
-      if (rank==1) printf("%2.1f  ",bt[j][i]);
+      if (rank==0) printf("%2.f  ",bt[j][i]);
     }
-    if (rank==1) printf("\n");
+    if (rank==0) printf("\n");
   }
 
 }
