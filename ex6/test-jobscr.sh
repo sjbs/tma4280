35ecfd76af15e9b4c997f2505b27bf5b5e6ae318
silas vm <silas.spence@gmail.com>
2013-03-29 22:02:18 +0100

    pragma in unpacking recbuf changed to 2nd loop to remove limit that p>=t for parallelizing that task.  jobscript file updated for testing on one kongull node.

--- ex6/test-jobscr.sh
+++ ex6/test-jobscr.sh
@@ -14,14 +14,20 @@ module load intelcomp
 module load openmpi/1.4.3-intel
 module load cmake
 KMP_AFFINITY="granularity=fine, compact"
-echo "serial:"
-./serial2k
-echo "omp 4 :"
-OMP_NUM_THREADS=4 ./omp2k
-echo "mpi 4:"
-mpirun -np 4 mpi-splitv
-echo "omp 16 :"
-OMP_NUM_THREADS=16 ./omp2k
-echo "mpi 16:"
-mpirun -np 16 mpi-splitv
+echo "testrun mpi 1:"
+export OMP_NUM_THREADS=1
+mpirun -np 1 4096
+echo "testrun mpi 12:"
+mpirun -np 12 4096
+echo "testrun omp 12:"
+export OMP_NUM_THREADS=12
+mpirun -np 1 4096
+echo "testrun mixed p=2,t=6:"
+export OMP_NUM_THREADS=6
+mpirun -np 2 4096
+echo "testrun mixed p=6,t=2:"
+export OMP_NUM_THREADS=2
+mpirun -np 6 4096
+
+
 
