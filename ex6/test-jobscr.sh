9c66527a0ffa0a7ce2e7287bc8bd57fe16231e74
silas <silas.spence@gmail.com>
2013-03-29 22:40:37 +0100

    fix jobscr mpi-poission call

--- ex6/test-jobscr.sh
+++ ex6/test-jobscr.sh
@@ -16,18 +16,18 @@ module load cmake
 KMP_AFFINITY="granularity=fine, compact"
 echo "testrun mpi 1:"
 export OMP_NUM_THREADS=1
-mpirun -np 1 4096
+mpirun -np 1 mpi-poisson 4096
 echo "testrun mpi 12:"
-mpirun -np 12 4096
+mpirun -np 12 mpi-poisson 4096
 echo "testrun omp 12:"
 export OMP_NUM_THREADS=12
-mpirun -np 1 4096
+mpirun -np 1 mpi-poisson 4096
 echo "testrun mixed p=2,t=6:"
 export OMP_NUM_THREADS=6
-mpirun -np 2 4096
+mpirun -np 2 mpi-poisson 4096
 echo "testrun mixed p=6,t=2:"
 export OMP_NUM_THREADS=2
-mpirun -np 6 4096
+mpirun -np 6 mpi-poisson 4096
 
 
 
